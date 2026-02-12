c1a330635925895611f2ee0c09d8eadb7b9e6b1c
TinzarTun <tinzartun2002@gmail.com>
2026-02-13 00:27:23 +0630

    role register form design completed

--- admin/role.php
+++ admin/role.php
@@ -51,19 +51,61 @@ if (isset($_POST['btnregister']))
     <?php include('sidebar.php'); ?>
 
     <div class="main-container">
-        <div class="pd-ltr-20">
+        <div class="pd-ltr-20 xs-pd-20-10">
+            <div class="min-height-200px">
+                <div class="page-header">
+                    <div class="row">
+                        <div class="col-md-6 col-sm-12">
+                            <div class="title">
+                                <h4>Role</h4>
+                            </div>
+                            <nav aria-label="breadcrumb" role="navigation">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                    <li class="breadcrumb-item"><a href="role_list.php">Role List</a></li>
+                                    <li class="breadcrumb-item active" aria-current="page">Role Register</li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Role Form Start -->
+                <div class="pd-20 card-box mb-30">
+                    <div class="clearfix">
+                        <div class="pull-left">
+                            <h4 class="text-blue h4">Registration Form</h4>
+                            <p class="mb-30">Please fill role information</p>
+                        </div>
+                        <div class="pull-right">
+                            <a href="role_list.php" class="btn btn-primary btn-sm scroll-click" role="button"><i class="icon-copy ti-angle-double-left"></i></a>
+                        </div>
+                    </div>
+
+                    <form action="role.php" method="post">
+                        <input type="hidden" name="txtRID" value="<?php echo AutoID('role', 'RoleID', 'RID-', 4) ?>">
+                        <div class="form-group">
+                            <label>Role</label>
+                            <input class="form-control" type="text" name="txtrole" placeholder="Enter Role Name" required>
+                        </div>
+
+                        <div class="clearfix">
+                            <div class="pull-left">
+                                <p class="mb-30 font-14"></p>
+                            </div>
+                            <div class="pull-right">
+                                <button class="btn btn-success" type="submit" name="btnregister">Register</button>
+                                <button class="btn btn-danger" type="reset" name="btncancel">Cancel</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <!-- Role Form End -->
+            </div>
+
             <?php include('footer.php'); ?>
         </div>
     </div>
-
-    <p>Please Fill Role Information</p>
-    <form action="role.php" method="post">
-        <input type="hidden" name="txtRID" value="<?php echo AutoID('role', 'RoleID', 'RID-', 4) ?>">
-        <label for="text">Role</label>
-        <input type="text" name="txtrole" placeholder="Enter Role Name" required>
-        <button type="submit" name="btnregister">Register</button>
-        <button type="reset" name="btncancel">Cancel</button>
-    </form>
 
      <?php include('script.php'); ?>
 </body>
