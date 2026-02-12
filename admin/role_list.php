230635c6fb22f53a2e0d69c377e67023089c3995
TinzarTun <tinzartun2002@gmail.com>
2026-02-13 00:35:43 +0630

    link with head

--- admin/role_list.php
+++ admin/role_list.php
@@ -8,9 +8,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>List</title>
+    <?php include('head.php'); ?>
 
     <script type="text/javascript" src="../includes/entryjs/jquery-3.1.1.slim.min.js"></script>
     <script type="text/javascript" src="../includes/DataTables/datatables.min.js"></script>
