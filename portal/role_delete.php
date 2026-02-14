f47d3702a447e784db86c21eb0ab19320aa532a9
TinzarTun <tinzartun2002@gmail.com>
2026-02-14 21:01:53 +0630

    fixed role delete

--- portal/role_delete.php
+++ portal/role_delete.php
@@ -3,7 +3,7 @@
     include("../includes/connect.php");
     include("../includes/AutoID_Functions.php");
     include("../includes/Browsing_Functions.php");
-    recordbrowse("http://localhost/UniEnroll/admin/role_delete.php");
+    recordbrowse("http://localhost/UniEnroll/portal/role_delete.php");
 
     if (isset($_REQUEST['rid'])) 
     {
