706e621fa2744bac0d76389a405781a46edd5ea9
TinzarTun <tinzartun2002@gmail.com>
2026-02-16 23:18:58 +0630

    link add

---
+++ portal/program.php
@@ -0,0 +1,8 @@
+<?php 
+    session_start();
+    include("../includes/connect.php");
+    include("../includes/AutoID_Functions.php");
+    include("../includes/Browsing_Functions.php");
+    recordbrowse("http://localhost/UniEnroll/portal/department.php");
+
+?>
