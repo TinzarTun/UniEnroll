0834f3a1d0a4c344cfdc0844ce80405c47d7de41
TinzarTun <tinzartun2002@gmail.com>
2026-02-20 19:37:31 +0630

    semester link fixed

---
+++ portal/semester_list.php
@@ -0,0 +1,142 @@
+<?php 
+    session_start();
+    include("../includes/connect.php");
+    include("../includes/Browsing_Functions.php");
+    recordbrowse("http://localhost/UniEnroll/portal/semester_list.php");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include('head.php'); ?>
+</head>
+<body>
+    <?php include('pre-loader.php'); ?>
+
+    <?php include('header.php'); ?>
+
+    <?php include('sidebar.php'); ?>
+
+    <div class="main-container">
+        <div class="pd-ltr-20 xs-pd-20-10">
+            <div class="min-height-200px">
+                <!-- Page Header Start -->
+                <div class="page-header">
+                    <div class="row">
+                        <div class="col-md-6 col-sm-12">
+                            <div class="title">
+                                <h4>Semester</h4>
+                            </div>
+                            <nav aria-label="breadcrumb" role="navigation">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                    <li class="breadcrumb-item active" aria-current="page">Semester List</li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+                <!-- Page Header End -->
+
+                <!-- Semester List Table Start -->
+                <div class="card-box mb-30">
+                    <div class="pd-20">
+                        <div class="clearfix">
+                            <div class="pull-left">
+                                <h4 class="text-blue h4">Semester List</h4>
+                                <p class="mb-0">List of semester information</p>
+                            </div>
+                            <div class="pull-right">
+                                <a href="semester.php" class="btn btn-primary btn-sm scroll-click" role="button"><i class="icon-copy ti-plus"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pb-20">
+                        <table class="data-table table stripe hover nowrap">
+                            <thead>
+                                <tr>
+                                    <th class="table-plus datatable-nosort">#</th>
+                                    <th>Program Name</th>
+                                    <th>Degree Level</th>
+                                    <th>Duration</th>
+                                    <th>Status</th>
+                                    <th class="datatable-nosort">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php 
+                                    $select=mysqli_query($connection,"SELECT 
+                                                                                        p.ProgramID,
+                                                                                        p.Program_Name,
+                                                                                        p.Degree_level,
+                                                                                        p.Duration_years,
+                                                                                        p.Status
+                                                                                    FROM program p
+                                                                                    JOIN department d 
+                                                                                        ON d.DepartmentID = p.DepartmentID");
+                                    $count=mysqli_num_rows($select);
+                                    for ($i = 0; $i < $count; $i++) 
+                                    {
+                                        $data = mysqli_fetch_array($select);
+                                        $pgid = $data['ProgramID'];
+                                        $pname = $data['Program_Name'];
+                                        $dlevel = $data['Degree_level'];
+                                        $duration = $data['Duration_years'];
+                                        $yearText = ($duration > 1) ? 'years' : 'year';
+                                        $status = $data['Status'];
+
+                                        echo "<tr>";
+                                            echo "<td class='table-plus'>$pgid</td>";
+                                            echo "<td>$pname</td>";
+                                            echo "<td>$dlevel</td>";
+                                            echo "<td>$duration $yearText</td>";
+                                                
+                                            if($status=="Active")
+                                            {
+                                                echo "<td><span class='badge badge-success'>Active</span></td>";
+                                            }
+
+                                            if($status=="Inactive")
+                                            {
+                                                echo "<td><span class='badge badge-secondary'>Inactive</span></td>";
+                                            }
+
+                                            echo "<td>
+                                                <div class='dropdown'>
+                                                    <a class='btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle' href='#' role='button' data-toggle='dropdown'>
+                                                        <i class='dw dw-more'></i>
+                                                    </a>
+
+                                                    <div class='dropdown-menu dropdown-menu-right dropdown-menu-icon-list'>
+                                                        <a class='dropdown-item' href='program_view.php?pgid=$pgid'><i class='dw dw-eye'></i> View</a>
+                                                        <a class='dropdown-item' href='program_update.php?pgid=$pgid'><i class='dw dw-edit2'></i> Edit</a>
+                                                        <a class='dropdown-item' href='program_delete.php?pgid=$pgid'><i class='dw dw-delete-3'></i> Delete</a>
+                                                    </div>
+                                                </div>
+                                            </td>";
+                                        echo "</tr>";
+                                    }
+                                 ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <!-- Semester List Table End -->
+            </div>
+            
+            <?php include('footer.php'); ?>
+        </div>
+    </div>
+    <?php include('script.php'); ?>
+    
+    <!-- buttons for Export datatable -->
+    <script src="src/plugins/datatables/js/dataTables.buttons.min.js"></script>
+    <script src="src/plugins/datatables/js/buttons.bootstrap4.min.js"></script>
+    <script src="src/plugins/datatables/js/buttons.print.min.js"></script>
+    <script src="src/plugins/datatables/js/buttons.html5.min.js"></script>
+    <script src="src/plugins/datatables/js/buttons.flash.min.js"></script>
+    <script src="src/plugins/datatables/js/pdfmake.min.js"></script>
+    <script src="src/plugins/datatables/js/vfs_fonts.js"></script>
+    <!-- Datatable Setting js -->
+    <script src="vendors/scripts/datatable-setting.js"></script></body>
+</html>
