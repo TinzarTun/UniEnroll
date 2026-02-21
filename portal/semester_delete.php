375e2b4421fb0d52eae17f0efe63598383af9b08
TinzarTun <tinzartun2002@gmail.com>
2026-02-21 22:48:06 +0630

    can delete semester

--- portal/semester_delete.php
+++ portal/semester_delete.php
@@ -4,4 +4,20 @@
     include("../includes/AutoID_Functions.php");
     include("../includes/Browsing_Functions.php");
     recordbrowse("http://localhost/UniEnroll/portal/semester_delete.php");
+
+if (isset($_REQUEST['smid'])) 
+{
+    $semesterID=$_REQUEST['smid'];
+
+    // safe delete
+    $delete="DELETE FROM semester
+             WHERE SemesterID='$semesterID'";
+    $run=mysqli_query($connection,$delete);
+            
+    if ($run) 
+    {
+        echo "<script>alert('Delete Successful!')</script>";
+        echo "<script>location='semester_list.php'</script>";
+    }
+}
 ?>
