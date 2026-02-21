c3911b4ff1285b525bd80f22f8db3493b9e1941d
TinzarTun <tinzartun2002@gmail.com>
2026-02-21 22:42:58 +0630

    link added

---
+++ portal/semester_delete.php
@@ -0,0 +1,7 @@
+<?php 
+    session_start();
+    include("../includes/connect.php");
+    include("../includes/AutoID_Functions.php");
+    include("../includes/Browsing_Functions.php");
+    recordbrowse("http://localhost/UniEnroll/portal/semester_delete.php");
+?>
